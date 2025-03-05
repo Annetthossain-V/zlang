5611cdd048dca9dd05eb1ea6e7d28db11e3e726e
Annett Hossain <annetthossain@outlook.com>
2025-03-05 20:36:37 +0600

    added rust

--- rust/runtime/target/debug/deps/runtime-03fd06f95553837a.d
+++
@@ -1,5 +0,0 @@
-/home/annetthossain/code/zlang/rust/runtime/target/debug/deps/libruntime-03fd06f95553837a.a: src/lib.rs
-
-/home/annetthossain/code/zlang/rust/runtime/target/debug/deps/runtime-03fd06f95553837a.d: src/lib.rs
-
-src/lib.rs:
--- rust/runtime/target/debug/libruntime.d
+++
@@ -1,1 +0,0 @@
-/home/annetthossain/code/zlang/rust/runtime/target/debug/libruntime.rlib: /home/annetthossain/code/zlang/rust/runtime/src/lib.rs
